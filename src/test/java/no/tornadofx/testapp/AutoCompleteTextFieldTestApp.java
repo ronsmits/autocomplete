e700d6fe7492a80457a72ecd00a50f56de174f01
Ron Smits <ron.smits@gmail.com>
2017-01-27 07:17:59 +0100

    bumped tornadofx version

--- src/test/java/no/tornadofx/testapp/AutoCompleteTextFieldTestApp.java
+++ src/test/java/no/tornadofx/testapp/AutoCompleteTextFieldTestApp.java
@@ -1,52 +1,47 @@
 package no.tornadofx.testapp;
 
-import java.io.InputStream;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import no.tornadofx.AutoCompleteTextField;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import java.io.InputStream;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * Created by ronsmi on 1/26/2017.
  */
-public class AutoCompleteTextFieldTestApp extends Application
-{
+public class AutoCompleteTextFieldTestApp extends Application {
     private JsonArray array;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         array = readFile();
 
         Function<String, List<String>> searcher = (String enteredText) -> array
-                .stream().map(it -> (JsonObject)it)
+                .stream().map(it -> (JsonObject) it)
                 .filter(listEntry -> listEntry.getString("name")
-                    .toLowerCase()
-                    .startsWith(enteredText.toLowerCase()))
+                        .toLowerCase()
+                        .startsWith(enteredText.toLowerCase()))
                 .map(o -> o.getString("name"))
                 .collect(Collectors.toList());
 
-                AutoCompleteTextField field = new AutoCompleteTextField(searcher);
-                Scene scene = new Scene(field);
-                primaryStage.setScene(scene);
-                primaryStage.show();
+        AutoCompleteTextField field = new AutoCompleteTextField(searcher);
+        Scene scene = new Scene(field);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    private JsonArray readFile()
-    {
+    private JsonArray readFile() {
         InputStream stream = getClass().getResourceAsStream("/countries.json");
         return Json.createReader(stream).readArray();
     }
